09952c1f4fb4923a3f59aee0cfabc918dc3c9785
Michael Anderson <Michael.Anderson@amesburytruth.com>
2018-06-14 12:39:04 -0500

    Fixed bug in string.prototype.padXXX

--- src/lib/string.ext.ts
+++ src/lib/string.ext.ts
@@ -4,14 +4,14 @@ interface String {
 }
 
 if ("padStart" in String.prototype === false) {
-    String.prototype.padStart = (targetLength: number, padString: string = " ") : string {
+    String.prototype.padStart = (targetLength: number, padString: string = " ") : string => {
         let result: string = this;
         while (result.length < targetLength) result = padString + result;
         return result;
     }
 }
 if ("padEnd" in String.prototype === false) {
-    String.prototype.padEnd = (targetLength: number, padString: string = " ") : string {
+    String.prototype.padEnd = (targetLength: number, padString: string = " ") : string => {
         let result: string = this;
         while (result.length < targetLength) result += padString;
         return result;
