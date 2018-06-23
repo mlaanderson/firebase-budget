288290804b51e96094fcadb06c96a4c953161cfe
Michael Anderson <Michael.Anderson@amesburytruth.com>
2018-06-23 07:08:02 -0500

    moving to the mac

--- iosapp/lib/regex.swift
+++ iosapp/lib/regex.swift
@@ -13,19 +13,20 @@ class Regex {
     let baseRegex: NSRegularExpression
 
     init(pattern: String, options: [NSRegularExpressionOption]) {
-        self.baseRegex = try! NSRegularExpression(pattern, options)
+        self.baseRegex = try! NSRegularExpression(pattern: pattern, options: options)
     }
 
-    func test(input: String) -> Bool {
+    func isMatch(input: String) -> Bool {
         let matches = self.baseRegex.matchesInString(input, options: nil, range: NSMakeRange(0, input.count))
         return matches.count > 0
     }
 
     func match(input: String) -> Match {
         // return the first match
+        let matches = self.baseRegex
     }
 
-    func matches(input: Stirng) -> MatchCollection {
+    func matches(input: String) -> MatchCollection {
         // return the matches
     }
 }
